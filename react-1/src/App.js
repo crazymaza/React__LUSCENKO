d568c6ab3620d28c01782bf9cc3bc8c231456fc3
Igor Valnev <dx89@mail.ru>
2020-02-28 18:27:23 +0300

    init: hw 2.

---
+++ react-1/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col col-lg-12">
+          <h1>First React APP</h1>
+          <img src="https://cdn0.iconfinder.com/data/icons/flat-round-system/512/reactos-256.png" alt="" />
+            <p>my first react app</p>
+            <button class="button-primary">Default</button>
+        </div>
+        </div>
+      </div>
+      );
+    }
+    
+    export default App;
