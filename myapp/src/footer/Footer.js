8633bf82c0c23e0a8d1c2dfd927263312197ed5a
Igor Valnev <dx89@mail.ru>
2020-03-05 15:29:28 +0300

    edit: hw 6.

--- myapp/src/footer/Footer.js
+++ myapp/src/footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 class Footer extends React.Component {
     render() {
         return (
-            <footer>
+            <footer style={{background : '#2d3346', color : '#fff'}}>
                 <p>Lorem, ipsum dolor.</p>
             </footer>
         );
