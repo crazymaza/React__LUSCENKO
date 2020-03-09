a23946619d01754a67412635320812ba829fd121
crazymaza <dx2389@gmail.com>
2020-03-09 23:14:51 +0300

    init: hw-9.

--- react-1/src/App.js
+++ react-1/src/App.js
@@ -1,37 +1,38 @@
 import React from 'react';
-import './App.css';
 
-function App() {
-	return (
-		<div class="container">
-			<div class="row">
-				<div class="col col-lg-12">
-					<h1>First React APP</h1>
-					<img src="https://cdn0.iconfinder.com/data/icons/flat-round-system/512/reactos-256.png" alt=""/>
-					<p>my first react app</p>
-					<button class="button-primary">Default</button>
-				</div>
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: '',
+			count: 0,
+		}
+	}
+
+	buttonClick = () => {
+		this.setState({text: 'Button work!'})
+	};
+
+	countUp = () => {
+		this.setState({count: this.state.count += 1});
+	};
+
+	resetCount = () => {
+		this.setState({count: 0});
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.buttonClick}>Button</button>
+				<p>{this.state.text}</p>
+				<button onClick={this.countUp}>Count</button>
+				<p>{this.state.count}</p>
+				<button onClick={this.resetCount}>ResetCount</button>
 			</div>
-            <div className="row">
-                <div className="col col-lg-12">
-                    <h2>About me</h2>
-                    <table>
-                        <th>
-                            Something about me
-                        </th>
-                        <tr>
-                            <td>Age</td>
-                            <td>30</td>
-                        </tr>
-                        <tr>
-                            <td>Country</td>
-                            <td>Russia</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-		</div>
-	);
+		);
+	}
+
 }
 
 export default App;
